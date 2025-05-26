3666e6419c0ff19a61203782f14bdd8b8f11fc84
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2025-05-26 09:47:25 -0500

    Bump version

--- src/agor/__init__.py
+++ src/agor/__init__.py
@@ -23,7 +23,7 @@ else:
         __version__ = version("agor")
     except PackageNotFoundError:
         # If all else fails, use hardcoded version
-        __version__ = "0.2.2"
+        __version__ = "0.2.3"
 
 __author__ = "Jeremiah K."
 __email__ = "jeremiahk@gmx.com"
